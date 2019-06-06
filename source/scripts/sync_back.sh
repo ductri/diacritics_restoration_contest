9bee707cc06f21755f58d0e9459ee440c5032529
ductri <ductricse@gmail.com>
2019-06-06 15:05:11 +0700

    ok

---
+++ source/scripts/sync_back.sh
@@ -0,0 +1,2 @@
+scp root@213.246.38.101:/root/code/trind/diacritics_restoration_contest/source/notebooks/* notebooks/
+scp root@213.246.38.101:/root/code/trind/diacritics_restoration_contest/source/main/vocab/output/* main/vocab/output/
--- sync_back.sh
+++
@@ -1,2 +0,0 @@
-scp root@213.246.38.101:/root/code/trind/diacritics_restoration_contest/source/notebooks/* source/notebooks/
-scp root@213.246.38.101:/root/code/trind/diacritics_restoration_contest/source/main/vocab/output/* source/main/vocab/output/
